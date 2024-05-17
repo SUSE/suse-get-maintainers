c3d6b43358a59a2d44c6a595c30bd7ac3c61cfc3
Miroslav Franc <mfranc@suse.cz>
2024-05-17 18:57:13 +0200

    suse-get-maintainers: bump to version 1.0.2

--- packaging/make_tarball.sh
+++ packaging/make_tarball.sh
@@ -3,9 +3,8 @@ VERSION=$(grep Version: suse-get-maintainers.spec | awk '{print $2}')
 NAME=$(grep Name: suse-get-maintainers.spec | awk '{print $2}')
 DIRECTORY="$NAME-$VERSION"
 
-mkdir -p "$DIRECTORY/src"
-cp "../$NAME.cc" ../git2.h ../helpers.h ../curl.h ../cves.h  ../Makefile ../suse-get-maintainers.1 "$DIRECTORY"
+mkdir "$DIRECTORY"
+cp "../$NAME.cc" ../maintainers.h ../git2.h ../helpers.h ../curl.h ../cves.h  ../Makefile ../suse-get-maintainers.1 "$DIRECTORY"
 gzip "$DIRECTORY/suse-get-maintainers.1"
-cp ../src/*.cc ../src/*.h "$DIRECTORY/src"
 tar cvJf "$DIRECTORY.tar.xz" "$DIRECTORY" && rm -rf "$DIRECTORY"
 
