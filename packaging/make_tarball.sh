2727ae9f6be7450db1244067badc497c473ac1e8
Miroslav Franc <mfranc@suse.cz>
2025-03-11 15:44:23 +0100

    suse-get-maintainers: fix missing header build failure

--- packaging/make_tarball.sh
+++ packaging/make_tarball.sh
@@ -6,6 +6,8 @@ DIRECTORY="$NAME-$VERSION"
 mkdir "$DIRECTORY"
 cp "../$NAME.cc" ../maintainers.h ../git2.h ../helpers.h ../curl.h ../cves.h  ../Makefile ../suse-get-maintainers.1 "$DIRECTORY"
 # TODO
+mkdir "$DIRECTORY/exp-suse-add-cves/"
+cp ../exp-suse-add-cves/cve2bugzilla.h "$DIRECTORY/exp-suse-add-cves/"
 cp ../temporary.h "$DIRECTORY"
 # END TODO
 gzip "$DIRECTORY/suse-get-maintainers.1"
