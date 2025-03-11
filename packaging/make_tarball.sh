f3d0e54c4a8fcd491bd05ef693377f8aac0a5d57
Miroslav Franc <mfranc@suse.cz>
2025-03-11 15:54:33 +0100

    suse-get-maintainers: fix the build failure properly

--- packaging/make_tarball.sh
+++ packaging/make_tarball.sh
@@ -4,10 +4,8 @@ NAME=$(grep Name: suse-get-maintainers.spec | awk '{print $2}')
 DIRECTORY="$NAME-$VERSION"
 
 mkdir "$DIRECTORY"
-cp "../$NAME.cc" ../maintainers.h ../git2.h ../helpers.h ../curl.h ../cves.h  ../Makefile ../suse-get-maintainers.1 "$DIRECTORY"
+cp "../$NAME.cc" ../maintainers.h ../git2.h ../helpers.h ../curl.h ../cves.h  ../Makefile ../suse-get-maintainers.1 ../cve2bugzilla.h "$DIRECTORY"
 # TODO
-mkdir "$DIRECTORY/exp-suse-add-cves/"
-cp ../exp-suse-add-cves/cve2bugzilla.h "$DIRECTORY/exp-suse-add-cves/"
 cp ../temporary.h "$DIRECTORY"
 # END TODO
 gzip "$DIRECTORY/suse-get-maintainers.1"
